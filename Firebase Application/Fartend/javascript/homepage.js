520364d2ac6cdd540a5796684a138adaccac551d
Eoin Cronly <eoin.cronly.2022@mumail.ie>
2023-12-05 13:11:28 +0000

    Fixed small errors

--- Firebase Application/Fartend/javascript/homepage.js
+++ Firebase Application/Fartend/javascript/homepage.js
@@ -6,31 +6,30 @@
 //the code below is for a manual slideshow
 var slideNum = 1;
 showSlide(slideNum);
-      
+
 function btnPressed(n) {
-    showSlide(slideNum += n);
+  showSlide((slideNum += n));
 }
    
 
 function showSlide(n) {
-    var slides = document.getElementsByClassName("pics");
-    
-    if (n > slides.length) {
-        slideNum = 1;
-    }
+  var slides = document.getElementsByClassName("pics");
 
-    if (n < 1) {
-        slideNum = slides.length;
-    }
-    
-    for (var i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
+  if (n > slides.length) {
+    slideNum = 1;
+  }
 
-    slides[slideNum-1].style.display = "block";  
+  if (n < 1) {
+    slideNum = slides.length;
+  }
+
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideNum - 1].style.display = "block";
 }
 
-/*
 //the code below is for an automatic slideshow
 let slideNum = 0;
 showSlides();
@@ -44,10 +43,10 @@ function showSlides() {
 
   slideNum++;
 
-  if (slideNum > slides.length){
+  if (slideNum > slides.length) {
     slideNum = 1; //reset to the first one
-}
-  slides[slideNum-1].style.display = "block";
+  }
+  slides[slideNum - 1].style.display = "block";
 
   setTimeout(showSlides, 2500); //change every 2.5 seconds
 }
