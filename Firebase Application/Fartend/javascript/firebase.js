7d3d5957028c788ce7967ae68c0401834dec519d
Eoin Cronly <eoin.cronly.2022@mumail.ie>
2023-12-11 11:34:42 +0000

    Revert "firebase be working again >life"
    This reverts commit 7196169b2d3f6d59944f22d3a9da06572800106e.

--- Firebase Application/Fartend/javascript/firebase.js
+++ Firebase Application/Fartend/javascript/firebase.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // firebase.js
 import firebaseConfig from "./firebaseConfig.js";
 import "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
@@ -9,3 +10,16 @@ const db = firebase.firestore();
 //const auth = firebase.auth();
 
 export { db, app };
+=======
+// firebase.js
+import firebaseConfig from "./firebaseConfig.js";
+import "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
+import "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export { db, app, auth };
+>>>>>>> parent of 7196169 (firebase be working again >life)
