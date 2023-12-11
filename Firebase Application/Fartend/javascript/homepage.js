d0ecaf3f7bff4737e7f654153461c80eb64abdd0
Eoin Cronly <eoin.cronly.2022@mumail.ie>
2023-12-11 11:34:42 +0000

    Revert "firebase be working again >life"
    This reverts commit 7196169b2d3f6d59944f22d3a9da06572800106e.

--- Firebase Application/Fartend/javascript/homepage.js
+++ Firebase Application/Fartend/javascript/homepage.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //the code below is for a manual slideshow
 /*var slideNum = 1;
 showSlide(slideNum);
@@ -44,3 +45,51 @@ function showSlides() {
 
   setTimeout(showSlides, 2500); //change every 2.5 seconds
 }
+=======
+//the code below is for a manual slideshow
+/*var slideNum = 1;
+showSlide(slideNum);
+
+function btnPressed(n) {
+  showSlide((slideNum += n));
+}
+
+function showSlide(n) {
+  var slides = document.getElementsByClassName("pics");
+
+  if (n > slides.length) {
+    slideNum = 1;
+  }
+
+  if (n < 1) {
+    slideNum = slides.length;
+  }
+
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideNum - 1].style.display = "block";
+} */
+
+//the code below is for an automatic slideshow
+let slideNum = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("pics");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideNum++;
+
+  if (slideNum > slides.length) {
+    slideNum = 1; //reset to the first one
+  }
+  slides[slideNum - 1].style.display = "block";
+
+  setTimeout(showSlides, 2500); //change every 2.5 seconds
+}
+>>>>>>> parent of 7196169 (firebase be working again >life)
