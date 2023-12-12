e64b267c7e93d8c01a9092c614a31530227e52e7
Fuad Dele-Ganiyu <fuad.deleganiyu.2022@mumail.ie>
2023-12-12 22:57:26 +0000

    few modifications

--- Firebase Application/Fartend/javascript/account.js
+++ Firebase Application/Fartend/javascript/account.js
@@ -1,3 +1,20 @@
+// Function to get saved profile data from localStorage
+function getProfileFromStorage() {
+    var savedName = localStorage.getItem('profileName');
+    var savedEmail = localStorage.getItem('profileEmail');
+
+    if (savedName && savedEmail) {
+        document.getElementById('name').innerText = savedName;
+        document.getElementById('email').innerText = savedEmail;
+    }
+}
+
+// Function to save updated profile data to localStorage
+function saveProfileToStorage(newName, newEmail) {
+    localStorage.setItem('profileName', newName);
+    localStorage.setItem('profileEmail', newEmail);
+}
+
 function editProfile() {
     var nameElement = document.getElementById('name');
     var emailElement = document.getElementById('email');
@@ -23,7 +40,12 @@ function saveProfile() {
     nameElement.innerHTML = newName;
     emailElement.innerHTML = newEmail;
 
+    saveProfileToStorage(newName, newEmail);
+
     var editButton = document.querySelector('.edit-profile button');
     editButton.innerText = 'Edit Profile';
     editButton.setAttribute('onclick', 'editProfile()');
 }
+
+// Load saved profile data when the page loads
+window.onload = getProfileFromStorage;
