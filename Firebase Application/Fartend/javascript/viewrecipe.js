c934e780a71cd3d10d65d1456396680b8d113f6f
Eoin Cronly <eoin.cronly.2022@mumail.ie>
2023-12-14 14:18:08 +0000

    new formatting regex

--- Firebase Application/Fartend/javascript/viewrecipe.js
+++ Firebase Application/Fartend/javascript/viewrecipe.js
@@ -1,5 +1,32 @@
 import { db } from "./firebase.js";
 
+// Helper function to insert line breaks dynamically
+function insertLineBreaks(text, maxLineLength) {
+  const words = text.split(/\s+/);
+
+  let currentLine = "";
+  const lines = [];
+
+  words.forEach((word) => {
+    const candidateLine =
+      currentLine.length === 0 ? word : `${currentLine} ${word}`;
+
+    if (candidateLine.length <= maxLineLength) {
+      currentLine = candidateLine;
+    } else {
+      lines.push(currentLine.trim());
+      currentLine = word;
+    }
+  });
+
+  // Add the last line if it's not empty
+  if (currentLine.trim() !== "") {
+    lines.push(currentLine.trim());
+  }
+
+  return lines.join("<br>");
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   // Get the internalID from the query parameters
   const urlParams = new URLSearchParams(window.location.search);
@@ -35,9 +62,15 @@ document.addEventListener("DOMContentLoaded", () => {
     // Handle the case where the internalID is not provided
   }
 });
-
 function displayRecipeDetails(recipe) {
   // Update specific elements with recipe details
+
+  // Insert line breaks dynamically for ingredients
+  const ingredientsWithLineBreaks = insertLineBreaks(recipe.ingredients, 50);
+
+  // Insert line breaks dynamically for instructions
+  const instructionsWithLineBreaks = insertLineBreaks(recipe.instructions, 50);
+
   document.getElementById(
     "recipeName"
   ).textContent = `Recipe Name: ${recipe.recipeName}`;
@@ -46,8 +79,10 @@ function displayRecipeDetails(recipe) {
   ).textContent = `Submitted By: ${recipe.username}`;
   document.getElementById(
     "ingredientsContainer"
-  ).innerHTML = `<h4>Ingredients:</h4>${recipe.ingredients}`;
-  document.getElementById("instructionsBox").textContent = recipe.instructions;
+  ).innerHTML = `<h4>Ingredients:</h4>${ingredientsWithLineBreaks}`;
+  document.getElementById(
+    "instructionsBox"
+  ).innerHTML = `<p>${instructionsWithLineBreaks}</p>`;
   document.getElementById(
     "prepTime"
   ).textContent = `Prep Time: ${recipe.preptime}`;
@@ -59,16 +94,8 @@ function displayRecipeDetails(recipe) {
     "allergens"
   ).textContent = `Allergens: ${recipe.dietaryTags.join(", ")}`;
   // Add more elements as needed
-
-  // If you want to update the entire HTML content, you can use the following:
-  // const recipeDetailsDiv = document.getElementById("recipe-details");
-  // recipeDetailsDiv.innerHTML = `
-  //   <h2>${recipe.recipeName}</h2>
-  //   <p><strong>Ingredients:</strong> ${recipe.ingredients}</p>
-  //   <p><strong>Instructions:</strong> ${recipe.instructions}</p>
-  //   <!-- Add more details as needed -->
-  // `;
 }
+
 document.getElementById("homebutton").addEventListener("click", () => {
   window.location.href = "index.html";
 });
